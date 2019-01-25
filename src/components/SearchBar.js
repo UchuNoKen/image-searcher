dcad0ef6989c643437bd4de65a0ccf2816dd13c7
UchuNoKen <xen.o.fite@gmail.com>
2019-01-25 14:36:48 -0500

    Added text input.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>Search bar</div>;
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
   }
 }
 
