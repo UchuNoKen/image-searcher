3a4b5eefc0f099725589f18f4e09b6ab5d8a6088
UchuNoKen <xen.o.fite@gmail.com>
2019-01-28 15:34:31 -0500

    Created api directory and file to handle get request.

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,24 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID dc4846cdeaf6444203f58a12988dc15a3dc669eb43e6ac2ff9892a166e3b9c37"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID dc4846cdeaf6444203f58a12988dc15a3dc669eb43e6ac2ff9892a166e3b9c37"
+      }
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
