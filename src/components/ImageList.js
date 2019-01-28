d46232c7bc389a8eaf39abaf2a83e2734f78fc5e
UchuNoKen <xen.o.fite@gmail.com>
2019-01-28 16:35:38 -0500

    Added css file for image styling.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,3 +1,4 @@
+import "./ImageList.css";
 import React from "react";
 
 const ImageList = props => {
@@ -5,7 +6,7 @@ const ImageList = props => {
     return <img key={id} alt={description} src={urls.regular} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
