72515ef2ed8eb3c372cf0ff5174234f83eb656be
UchuNoKen <xen.o.fite@gmail.com>
2019-01-28 13:46:27 -0500

    Added search submit handler to App component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,10 +6,15 @@ class SearchBar extends React.Component {
   //   console.log(event.target.value);
   // }
 
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
